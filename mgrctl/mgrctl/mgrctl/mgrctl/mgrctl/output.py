a8e2773976d660a21dd7fcde8b170b591dd8d106
QinYuYuanHan <2271610676@qq.com>
2026-04-21 00:48:10 +0800

    Delete mgrctl/mgrctl/mgrctl/mgrctl/mgrctl directory

--- mgrctl/mgrctl/mgrctl/mgrctl/mgrctl/output.py
+++
@@ -1,8 +0,0 @@
-import json
-import yaml
-
-def render_output(data, fmt):
-    if fmt == "json":
-        print(json.dumps(data, indent=2))
-    elif fmt == "yaml":
-        print(yaml.dump(data, sort_keys=False))
